ed744aefdf8795286b24700201fe634bb1a439e0
sanskargit <sanskar.dixit@yendigial.com>
2024-02-25 18:37:50 +0530

    25-02-2024 this is tutorial

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Title = () => 
+{
+    return (
+        <h1>
+            React is happening
+        </h1>
+    )
+}
+const temp = <h2>hello </h2>
 
-const heading = React.createElement("h1",{id : "heading"},"it is a heading tag");
+const HeadingComponent = ()=>
+{
+    return  (<div id ="container">
+       <Title/>
+       {Title()} 
+       {temp}
+    <h1 className ="heading">
+        hi this the H1 
+    </h1>
+
+</div>);
+    
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);
+
 
